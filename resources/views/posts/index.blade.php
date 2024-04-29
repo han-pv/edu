d97180674917914509b94dcb8fbbaddd55dcbdd9
han-pv <pirliyev17@gmail.com>
2024-04-29 17:56:57 +0500

    1.15.0

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,19 +5,26 @@
 @section('content')
     <div class="container-xl">
         <div class="h3 fw-bold text-success my-4">
-            Teachers
+            Posts
         </div>
             <div class="col-12 my-4">
-                <div class="row g-3">
-                    @forelse($teachers as $teacher)
-                        @include('app.teacher')
-                    @empty
-                        <div class="col-12">
-                            <div class="fs-1 text-center rounded border p-5">
-                                Teacher not found
+                <div class="row row-cols-2 g-5">
+                    @foreach($posts as $post)
+                        <div class="col">
+                            <div class=" border bg-dark bg-opacity-10 h-100 p-3">
+                                <div class="col">
+                                    <div class="h4 ">{{$post->title}}</div>
+                                    <div class="h6 text-secondary ">{{$post->content}}...</div>
+                                    <div class="d-flex">
+                                        <div class="text-success small">View: {{$post->view_count}}</div>
+                                        <div class="text-success small ms-3">Like: {{$post->like_count}}</div>
+                                    </div>
+
+                                </div>
                             </div>
                         </div>
-                    @endforelse
+                    @endforeach
                 </div>
+            </div>
             </div>
 @endsection
